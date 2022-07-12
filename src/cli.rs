f7e0d2c3ba88cf00f2de1408a2f2f7a35d6c47b1
Kees van Voorthuizen <me@keesvv.nl>
2022-07-12 23:00:49 +0200

    feat(cli): make headings bold

--- src/cli.rs
+++ src/cli.rs
@@ -1,9 +1,14 @@
+use colored::*;
 use kash::statement::FixedStatement;
 use tabular::{Row, Table};
 
+pub fn format_heading(heading: &str) -> String {
+    String::from(heading).bold().to_string()
+}
+
 pub fn format_fixed(statements: Vec<FixedStatement>) -> Table {
     let mut table = Table::new("{:<} {:<} {:>}")
-        .with_heading("Fixed costs")
+        .with_heading(format_heading("Fixed costs"))
         .with_row(Row::from_cells(["tag", "description", "avg/mo"]));
 
     for statement in statements {
