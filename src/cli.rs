e6e8d1694033a46eb3834b3559562daa6ab0b71e
Kees van Voorthuizen <me@keesvv.nl>
2022-07-12 23:01:05 +0200

    feat(cli): add totals row for fixed table

--- src/cli.rs
+++ src/cli.rs
@@ -11,13 +11,24 @@ pub fn format_fixed(statements: Vec<FixedStatement>) -> Table {
         .with_heading(format_heading("Fixed costs"))
         .with_row(Row::from_cells(["tag", "description", "avg/mo"]));
 
-    for statement in statements {
+    for statement in &statements {
         table.add_row(Row::from_cells([
-            statement.tag,
-            statement.description,
+            statement.tag.clone(),
+            statement.description.clone(),
             statement.costs.month_avg().to_string(),
         ]));
     }
+
+    table.add_row(Row::from_cells([
+        "total",
+        "",
+        &statements
+            .iter()
+            .map(|s| s.costs.month_avg())
+            .sum::<f32>()
+            .to_string()
+            .as_str(),
+    ]));
 
     table
 }
