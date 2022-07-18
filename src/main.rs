ec24002a5f8c1494ecaa4df40aacb87a04b78ae7
Kees van Voorthuizen <me@keesvv.nl>
2022-07-18 16:07:27 +0200

    feat(cli): add arg parsing & -j flag

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,22 @@
 mod cli;
 
-use cli::display::StatementsTable;
+use cli::display::{StatementsJson, StatementsTable};
 use cli::format::{Deserializer, Statement};
+use std::env;
 use std::io;
 
 fn main() {
     let statements = io::stdin()
         .lines()
-        .map(|ln| Deserializer::from_str(&ln.unwrap()).deserialize().unwrap())
-        .collect::<Vec<Statement>>();
+        .map(|ln| Deserializer::from_str(&ln.unwrap()).deserialize().unwrap());
+    let collect_st = || statements.collect::<Vec<Statement>>();
 
-    print!("{}", StatementsTable::new(&statements));
+    // TODO: use clap
+    match env::args().nth(1) {
+        Some(e) => match e.as_str() {
+            "-j" => println!("{}", StatementsJson::new(&collect_st()).to_string()),
+            "-h" | _ => eprintln!("usage: {} [-h|-j]", env::args().nth(0).unwrap()),
+        },
+        _ => println!("{}", StatementsTable::new(&collect_st()).to_string()),
+    }
 }
