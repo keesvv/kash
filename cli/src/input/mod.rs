6f81c0a2b26f7bca04ca2d9d01472f5d412f78d6
Kees van Voorthuizen <me@keesvv.nl>
2022-07-19 20:40:53 +0200

    feat(input): add Input trait & InputError enum

--- cli/src/input/mod.rs
+++ cli/src/input/mod.rs
@@ -1,1 +1,15 @@
 pub mod ktf;
+
+use self::ktf::Statement;
+use std::io::Read;
+
+#[derive(Debug)]
+pub enum InputError {
+    Invalid(String),
+}
+
+pub trait Input {
+    fn from_read<R>(&self, reader: R) -> Result<Vec<Statement>, InputError>
+    where
+        R: Read;
+}
