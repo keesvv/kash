757680b6fe95479291e64954665c0c6781713f0b
Kees van Voorthuizen <me@keesvv.nl>
2022-07-25 19:22:27 +0200

    test(lib): add de_header_single_col test

--- ktf/src/lib.rs
+++ ktf/src/lib.rs
@@ -6,3 +6,15 @@ mod serde;
 
 #[cfg(feature = "serde")]
 pub use self::serde::*;
+
+#[cfg(test)]
+mod tests {
+    use super::de::Deserializer;
+    use super::error::Result;
+
+    #[test]
+    fn de_header_single_col() -> Result<()> {
+        let mut des = Deserializer::from_str(">col1");
+        Ok(assert_eq!(vec!["col1"], des.parse_header()?))
+    }
+}
