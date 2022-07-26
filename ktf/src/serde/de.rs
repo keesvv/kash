7c0ae8a4ed7c60802347a3d553d2e12f11932cb9
Kees van Voorthuizen <me@keesvv.nl>
2022-07-26 19:13:15 +0200

    feat(ktf): add support for enum identifiers

--- ktf/src/serde/de.rs
+++ ktf/src/serde/de.rs
@@ -47,6 +47,13 @@ impl<'a, 'de> de::Deserializer<'de> for &'a mut Deserializer<'de> {
         visitor.visit_seq(LineSeq::new(self))
     }
 
+    fn deserialize_identifier<V>(self, visitor: V) -> Result<V::Value>
+    where
+        V: Visitor<'de>,
+    {
+        visitor.visit_string(self.parse_string()?)
+    }
+
     fn deserialize_f32<V>(self, visitor: V) -> Result<V::Value>
     where
         V: Visitor<'de>,
@@ -64,7 +71,7 @@ impl<'a, 'de> de::Deserializer<'de> for &'a mut Deserializer<'de> {
     forward_to_deserialize_any! {
         bool i8 i16 i32 i64 i128 u8 u16 u32 u64 u128 f64 char str
         bytes byte_buf option unit unit_struct newtype_struct tuple
-        tuple_struct struct enum identifier ignored_any
+        tuple_struct struct enum ignored_any
     }
 }
 
