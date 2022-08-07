0d7ed16753b2af748865b99b2f25390d4c6be811
Kees van Voorthuizen <me@keesvv.nl>
2022-08-07 13:53:29 +0200

    feat(transaction): add custom Date ser/de

--- lib/src/date.rs
+++ lib/src/date.rs
@@ -1,4 +1,25 @@
-use serde::{Deserialize, Serialize};
+use chrono::{DateTime, FixedOffset};
+use serde::{de, Deserialize, Serialize};
 
-#[derive(Deserialize, Serialize, Clone, Debug)]
-pub struct Date(String);
+#[derive(Clone, Debug)]
+pub struct Date(DateTime<FixedOffset>);
+
+impl<'de> Deserialize<'de> for Date {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        let raw_date = String::deserialize(deserializer)?;
+        let date = DateTime::parse_from_rfc3339(&raw_date).map_err(de::Error::custom)?;
+        Ok(Self(date))
+    }
+}
+
+impl Serialize for Date {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        serializer.serialize_i64(self.0.timestamp())
+    }
+}
