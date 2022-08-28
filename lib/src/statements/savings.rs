2126faa62e43976d37afe3ac92954db014d8c9c3
Kees van Voorthuizen <me@keesvv.nl>
2022-08-28 14:26:30 +0200

    feat(lib/savings): add recurring savings model

--- lib/src/statements/savings.rs
+++ lib/src/statements/savings.rs
@@ -1,4 +1,6 @@
+use chrono::{NaiveDate, Utc};
 use serde::{ser::SerializeMap, Deserialize, Serialize};
+use std::cmp::Ordering;
 
 #[derive(Deserialize, Clone, Debug)]
 pub struct Goal {
@@ -46,8 +48,33 @@ pub struct Savings {
     pub goal_id: Option<String>,
 }
 
+impl Savings {
+    pub fn get_total_amount(&self) -> f32 {
+        match self.model {
+            SavingsModel::Single => self.amount,
+            SavingsModel::Recurring {
+                start_date,
+                end_date,
+            } => {
+                let diff = end_date.unwrap_or(Utc::now().date_naive()) - start_date;
+                self.amount
+                    * (match (diff.num_weeks() % 4).cmp(&0) {
+                        Ordering::Equal => diff.num_weeks(),
+                        Ordering::Greater => (diff.num_weeks() - diff.num_weeks() % 4),
+                        Ordering::Less => 0,
+                    } / 4) as f32
+            }
+        }
+    }
+}
+
 #[derive(Deserialize, Serialize, Clone, Debug)]
 #[serde(rename_all = "camelCase", tag = "model")]
 pub enum SavingsModel {
     Single,
+    #[serde(rename_all = "camelCase")]
+    Recurring {
+        start_date: NaiveDate,
+        end_date: Option<NaiveDate>,
+    },
 }
